9b9a3113ad444501291b2fb2d48ab47f7de352f7
Fabian <96124427+ByteMirror@users.noreply.github.com>
2026-04-15 20:24:59 +0200

    chore: bump valkenhall to v0.9.2

--- Casks/valkenhall.rb
+++ Casks/valkenhall.rb
@@ -1,6 +1,6 @@
 cask "valkenhall" do
   version "0.9.2"
-  sha256 "d817ae84b62c6df92882f49064ff3679b20dc78e998d65f3bb157cf59cfb058f"
+  sha256 "a0a9e4ca1c3c477ec3ca4fbb546b8ef17ced3c3199f7995e19dc638a4608ac56"
 
   url "https://github.com/ByteMirror/valkenhall/releases/download/v#{version}/stable-macos-arm64-Valkenhall.dmg"
   name "Valkenhall"
