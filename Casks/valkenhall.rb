5db9c1cdd83493d0a5eabf521a7b0573411d43e5
Fabian <96124427+ByteMirror@users.noreply.github.com>
2026-04-19 18:12:44 +0200

    chore: bump valkenhall to v0.12.0

--- Casks/valkenhall.rb
+++ Casks/valkenhall.rb
@@ -1,8 +1,8 @@
 cask "valkenhall" do
-  version "0.11.4"
-  sha256 "ccf91b52c84f657d61f8747bda1121897a9571326332942eec5414b5aacd17b4"
+  version "0.12.0"
+  sha256 "d23855b8cae495caefac04e6a56992d90f6f29c35b52c70c8fcc3c2e4a8c9876"
 
-  url "https://github.com/ByteMirror/valkenhall/releases/download/v#{version}/Valkenhall-0.11.4-arm64.dmg"
+  url "https://github.com/ByteMirror/valkenhall/releases/download/v#{version}/Valkenhall-0.12.0-arm64.dmg"
   name "Valkenhall"
   desc "Desktop card game arena for Sorcery TCG"
   homepage "https://github.com/ByteMirror/valkenhall"
